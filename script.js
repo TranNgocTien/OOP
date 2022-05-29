dfb9b7e94ae1ba9430ee9b44bc6263fd6eea587d
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-05-29 20:47:30 +0700

    Prototypal Inheritance and the prototype chain

--- script.js
+++ script.js
@@ -37,3 +37,23 @@ Person.prototype.species = 'Homo Spaiens';
 console.log(jonas.species, matilda.species);
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species')); 
+
+console.log(jonas.__proto__);
+
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+console.log(Person.prototype.constructor);
+
+const arr=[7,2,9,4,6,6,7,6,9];
+console.log(arr.__proto__);
+
+console.log(arr.__proto__=== Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+Array.prototype.unique= function(){
+    return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+
