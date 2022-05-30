677d0a4148fab385b55f672d8e4fbadeaa8d9456
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-05-30 19:18:04 +0700

    Setters and Getters

--- script.js
+++ script.js
@@ -89,8 +89,8 @@
 
 //Class declaration
 class PersonCl{
-    constructor(firstName,birthYear){
-        this.firstName=firstName;
+    constructor(fullName,birthYear){
+        this.fullName=fullName;
         this.birthYear=birthYear;
     }
 
@@ -98,11 +98,30 @@ class PersonCl{
     calcAge(){
         console.log(2037-this.birthYear);
     }
+
+    greet(){
+        console.log(`Hey ${this.firstName}`);
+    }
+
+    get age(){
+        return 2037-this.birthYear;
+    }
+
+    //Set a property that already exists
+    set fullName(name){
+        if(name.includes(' ')) this.fullName=name;
+        else alert(`${name} is not a full name!`);
+    }
+    get fullName(){
+        return this.__fullName;
+    }
 }
 
-const jessica=new PersonCl('Jessica',1996);
+const jessica=new PersonCl('Jessica Davis',1996);
 console.log(jessica);
 jessica.calcAge();
+
+console.log(jessica.age);
 console.log(jessica.__proto__===PersonCl.prototype);
 
 PersonCl.prototype.greet=function(){
@@ -110,3 +129,11 @@ PersonCl.prototype.greet=function(){
 }
 jessica.greet();
 
+const account={
+    owner:'jonas',
+    movements:[200,530,120,300],
+
+    get latest(){
+        return this.movements.slice(-1).pop()
+    }
+}
