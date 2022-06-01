cfa818a089dc070f71fd7327748d612b94024d1e
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-06-02 00:02:31 +0700

    Encapsulation: Private class fields and methods

--- script.js
+++ script.js
@@ -387,36 +387,58 @@ jay.init('Jay',2010,'Computer Science')
 jay.introduce();
 jay.calcAge();
 
+
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+
 class Account{
+    // 1) Public fields (instances)
+    locale= navigator.language;
+    
+    // 2) Private fields
+    #movements=[];
+    #pin;
     constructor(owner,currency,pin){
         this.owner=owner;
         this.currency=currency;
 
         //Protected property
-        this._pin=pin;
-        this._movements=[];
+        this.#pin=pin;
+        // this._movements=[];
         // this.locale=navigator.language;
         console.log(`Thanks for opening an account, ${owner}`);
     }
+
+    //3) Public methods
     //Public interface
     getMovements(){
-        return this._movements;
+        return this.#movements;
     }
     deposit(val){
-        this._movements.push(val);
+        this.#movements.push(val);
     }
 
     withdraw(val){
         this.deposit(-val);
-    }
-    _approveLoan(val){
-        return true;
     }
     requestLoan(val){
         if(this._approveLoan(val)){
             this.deposit(val);
             console.log(`Loan approved`);
         }
+    }
+
+    static helper(){
+        console.log('Helper');
+    }
+
+    //4) Private methods
+    
+    _approveLoan(val){
+        return true;
     }
 }
 
@@ -429,5 +451,5 @@ acc1.withdraw(140);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
 console.log(acc1);
-console.log(acc1.pin);
+
 
